904ea75317db525b14e41f80c7eca09a5bbf3802
adam <ibrahimadam193@gmail.com>
2017-05-07 00:35:31 -0400

    Change how db is created in app.
    Add some environment variables to app.yaml that must be accessed in
    app.py for connecting to the GCP SQL server.
    
    Change requirements.txt to remove unnecssary packages.
    
    Changes to be committed:
          modified:   app.py
          modified:   app.yaml
          modified:   requirements.txt

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import MySQLdb
 from flask import Flask, request, render_template
 import datetime
 import calendar
+# For examining the environment of the interpreter.
+import os
 
 def add_days(sourcedate, months, days):
     month = sourcedate.month - 1 + months
@@ -13,10 +15,46 @@ def add_days(sourcedate, months, days):
 
 app = Flask(__name__)
 
+# Source: https://cloud.google.com/appengine/docs/standard/python/cloud-sql/
+def connect_to_cloudsql():
+    # These environment variables are configured in app.yaml.
+    CLOUDSQL_CONNECTION_NAME = os.environ.get('CLOUDSQL_CONNECTION_NAME')
+    CLOUDSQL_USER = os.environ.get('CLOUDSQL_USER')
+    CLOUDSQL_PASSWORD = os.environ.get('CLOUDSQL_PASSWORD')
+
+    # When deployed to App Engine, the `SERVER_SOFTWARE` environment variable
+    # will be set to 'Google App Engine/version'.
+    if os.getenv('SERVER_SOFTWARE', '').startswith('Google App Engine/'):
+        # Connect using the unix socket located at
+        # /cloudsql/cloudsql-connection-name.
+        cloudsql_unix_socket = os.path.join(
+            '/cloudsql', CLOUDSQL_CONNECTION_NAME)
+
+        db = MySQLdb.connect(
+            unix_socket=cloudsql_unix_socket,
+            user=CLOUDSQL_USER,
+            passwd=CLOUDSQL_PASSWORD)
+
+    # If the unix socket is unavailable, then try to connect using TCP. This
+    # will work if you're running a local MySQL server or using the Cloud SQL
+    # proxy, for example:
+    #
+    #   $ cloud_sql_proxy -instances=your-connection-name=tcp:3306
+    #
+    # If this was started using dev_appserver.py or on GCP
+    elif CLOUDSQL_USER and CLOUDSQL_PASSWORD:
+        db = MySQLdb.connect(
+            host='127.0.0.1', user=CLOUDSQL_USER, passwd=CLOUDSQL_PASSWORD)
+    # If this is being run on Laisa's computer.
+    else:
+        db = MySQLdb.connect("localhost", "root", "", "team1")  # connects to the database
+
+    return db
 
 @app.route('/', methods=['get', 'post'])
 def home():  # index page index.html
-    db = MySQLdb.connect("localhost", "root", "", "team1")  # connects to the database
+
+    db = connect_to_cloudsql()
     ob = db.cursor()
     query = 'SELECT stations.station_name FROM s17336team1.stations'
     ob.execute(query)
