04f6ec933204a367e1a755633fabbe5778371c56
adam <ibrahimadam193@gmail.com>
2017-05-06 22:59:44 -0400

    Add files to make project work with Google Cloud Platform.
    - app.yaml: Project metadata that GCP needs to be aware of.
    - appengine_config.py: A python file which specifies a directory
    (possibly more than one) where other third-party python libraries are
    located (the libraries you'd install from pip, or something like that).
    - requirements.txt: A list of 3rd party packages being used. To create
    this, run the command `pip freeze` and place the output from that
    command in this file. This isn't strictly necessary, but it makes
    keeping which packages are installed easy to keep track of.
    
    Modified the .gitignore file to ignore the directory `lib`, which is
    necessary for GCP's third party libraries, but not necessary anywhere
    else.
    
    Changes to be committed:
          new file:   app.yaml
          new file:   appengine_config.py
          new file:   requirements.txt
          modified:   .gitignore

---
+++ appengine_config.py
@@ -0,0 +1,8 @@
+# Written Tutorial Line:
+# from google.appengine.ext import vendor
+# Interactive Tutorial Line:
+import vendor
+
+# Adds the folder ./lib the the list of places where python libraries
+# are found. The folder needs to exist for this to work, of course.
+vendor.add('lib')
